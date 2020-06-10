81d28a8e9cc633e171ce0c52a26ede6d48de1719
Gopikrishna Pentyala <gopikrishna.pentyala@nl.abnamro.com>
2020-06-10 19:24:46 +0200

    adding conditions for web url

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ const http = require('http')
 
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.end('Hello Nodejs');
+    if(req.url === '/about'){
+        res.end(`this is about page`);
+    } else if(req.url === '/contact'){
+        res.end(`this is contact page`);
+    } else if(req.url === '/'){
+        res.end(`this is home page`);
+    }else{
+        res.writeHead(404);
+        res.end('page not found')
+    }
 })
 
 server.listen(3000);
